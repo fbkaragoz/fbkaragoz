22741770ea77a285eadc424473b73fcabfade56c
rekurrenzk <Fatihburak@protonmail.com>
2023-12-27 12:40:24 +0300

    update

--- acr.c
+++ acr.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <math.h>
+
 
 #define WIDTH 50
 #define HEIGHT 30
@@ -9,6 +11,7 @@
 int main() {
     char screen[WIDTH * HEIGHT];
     const char* logo = "CDLI";
+    int frame = 0;
     int logoLength = strlen(logo);
     int logoPosition = WIDTH;
 
@@ -20,6 +23,14 @@ int main() {
             if (x >= 0 && x < WIDTH){
                 screen[HEIGHT / 2 * WIDTH + x] = logo[i];
             }
+        for (int x = 0; x<WIDTH; ++x){
+            int waveHeight = (sin(frame * 0.1 + x * 0.2) + 1) * (HEIGHT / 3);
+            int y = HEIGHT / 2 + waveHeight;
+
+            if (y >= 0 && y < HEIGHT) {
+                screen[y * WIDTH + x] = '*';
+            }
+        }
         }
         printf("\033[H");
         for (int i = 0; i<HEIGHT; i++){
