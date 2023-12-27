81a124634e520d9157628b84dda1dca0d90b5ae2
rekurrenzk <Fatihburak@protonmail.com>
2023-12-27 13:26:58 +0300

    update

--- acr.c
+++ acr.c
@@ -23,16 +23,26 @@ int main() {
             if (x >= 0 && x < WIDTH){
                 screen[HEIGHT / 2 * WIDTH + x] = logo[i];
             }
+
         for (int x = 0; x<WIDTH; ++x){
             int waveHeight = (sin(frame * 0.1 + x * 0.2) + 1) * (HEIGHT / 3);
             int y = HEIGHT / 2 + waveHeight;
+            if (y >= 0 && y < HEIGHT) {
+                screen[y * WIDTH + x] = '*';
+            }
+            y = HEIGHT / 2 - waveHeight;
+            for (int x = 0; x<WIDTH; ++x){
+            int waveHeight = (sin(frame * 0.1 + x * 0.2) + 1) * (HEIGHT / 3);
+            int y = HEIGHT / 2 + waveHeight;
 
+            }
             if (y >= 0 && y < HEIGHT) {
                 screen[y * WIDTH + x] = '*';
             }
         }
         }
         printf("\033[H");
+
         for (int i = 0; i<HEIGHT; i++){
             for (int j = 0; j<WIDTH; j++){
                 putchar(screen[i * WIDTH + j]);
